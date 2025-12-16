bfafd56d91cb4d4ee87ec459ff147af23676fd6d
Saad <saadalabeed1737@gmail.com>
2025-12-16 17:08:18 -0500

    First commit (Implemented the file structure)

---
+++ build/CMakeFiles/dispatch_game.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dispatch_game.dir/link.d"
+  "CMakeFiles/dispatch_game.dir/src/main.cpp.o"
+  "CMakeFiles/dispatch_game.dir/src/main.cpp.o.d"
+  "dispatch_game"
+  "dispatch_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dispatch_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
